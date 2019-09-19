8f54bb77700c94b7fee8e24dc519395050458247
nhann <nhanndeh@yahoo.com>
2019-09-19 09:18:31 -0500

    Added a print statement when read status changes

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ def search_book():
     matches = store.book_search(search_term)
     ui.show_books(matches)
 
- fix_crash
+
 ##added try catch block if error occurs when trying to change read status of book
-=======
+
 
 ##Added Delete book function here
 def delete_book():
@@ -69,9 +69,11 @@ def delete_book():
     store.delete_book(book)
     ui.message('Book Deleted')
 
- master
+
 def change_read():
-     try:
+
+    try:
+
         book_id = ui.get_book_id()
         book = store.get_book_by_id(book_id)  
         new_read = ui.get_read_value()     
@@ -80,6 +82,7 @@ def change_read():
     except:
         ui.message('Book not found')
 
+        print("The book has been read already !")
     
 
 def quit_program():
